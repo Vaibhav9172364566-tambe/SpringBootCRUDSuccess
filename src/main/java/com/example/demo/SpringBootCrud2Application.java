741f5fcc3e2e50038df3143a3c098a7e54d144cc
Vaibhav Tambe <tambevaibhav207@gmail.com>
2024-07-22 11:40:33 +0530

    perform select,Delet,update operation perform

--- src/main/java/com/example/demo/SpringBootCrud2Application.java
+++ src/main/java/com/example/demo/SpringBootCrud2Application.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -20,22 +22,90 @@ public class SpringBootCrud2Application implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		User user1=new User("Vaibhav","abc@gmail.com","Male","Pune");
-		 User user2=new User("Tambe","tambe@gmail.com", "Male", "Nashik");
 		
+		
+		//INSERTION 
+		
+		
+		
+//		User user1=new User("Vaibhav","abc@gmail.com","Male","Pune");
+//		 User user2=new User("Tambe","tambe@gmail.com", "Male", "Nashik");
+//		
 //		user1.setName("Vaibhav");
 //		user1.setEmail("abc@gmail.com");
 //		user1.setGender("Male");
 //		user1.setCity("pune");
+//		
+//		boolean status=	userDao.insertuser(user1);
+//		if(status) {
+//			System.out.println("User inserted succefully");
+//		}
+//		else 			System.out.println("User Failed not insert data");
+//		}
+//		
+//		
 		
-		boolean status=	userDao.insertuser(user1);
-		if(status) {
-			System.out.println("User inserted succefully");
-		}
-		else {
-			System.out.println("User Failed not insert data");
-		}
+		
+		// UPDATION METHOD 
+		
+//		User user3=new User("Abhi", "abc@gmail.com", "Male", "Nashik");
+//		
+//	boolean status=	userDao.updateUser(user3);
+//	if(status) {
+//		System.out.println(" Updation Success");
+//	}
+//	else {
+//		System.out.println(" Updation Fail");
+//	}
+//		
+//		
 	
-	}
+	
+	//DELETE
+		
+		 
+//	boolean status=	userDao.deleteUserByEmail("abc@gmail.com");
+//		if(status) {
+//			System.out.println("Deletion success");
+//		}
+//		else {
+//			System.out.println("Deletion fail");
+//		}
+//		
+//	
+//	
+	
+	
+	
+		//Select 
+		
+//	User user=	userDao.getUserByEamil("abc@gmail.com");
+//		
+//		System.out.println("Name : " +user.getName());
+//		System.out.println("Email : " +user.getEmail());
+//		System.out.println("Gender : " +user.getGender());
+//		System.out.println("City : " +user.getCity());
+//	
+//	
+	
+		
+		
+		
+		
+		
+		//select all user
+     List<User> list=userDao.getAllUsers();		
+     for(User user:list) {
+    	 System.out.println("Name : " +user.getName()); 	
+    	 System.out.println("Email : " +user.getEmail());
+		System.out.println("Gender : " +user.getGender());
+		System.out.println("City : " +user.getCity());
+	  System.out.println(" ===================================");
 
+    	 
+     }
+		
+		
+	}
 }
+
--- src/main/java/com/example/demo/dao/Dao.java
+++ src/main/java/com/example/demo/dao/Dao.java
@@ -1,7 +1,12 @@
 package com.example.demo.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.User;
@@ -34,5 +39,92 @@ public class Dao {
 			}
         	   return status;
            }
+           
+           public boolean updateUser(User user) {
+        	   
+        	 boolean  status=false;
+        	  try {
+        		  String updatesql = "UPDATE user SET  name=?, gender=?, city=? where email=?";
+        		  int count = jdbcTemplate.update(updatesql, user.getName(), user.getGender(), user.getCity(),user.getEmail());
+
+                    
+                  if(count>0) {
+                	  status=true;
+                  }
+                  else {
+                	  status=false;
+                  }
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+				status=false;
+				e.printStackTrace();
+			}
+        	  return status;
+           }
+           
+           
+           //DELETE
+           public boolean deleteUserByEmail(String email) {
+        	   
+          	 boolean  status=false;
+          	  try {
+          		  String deletesql = "delete from user where email=?";
+          		  int count = jdbcTemplate.update(deletesql,email);
+
+                      
+                    if(count>0) {
+                  	  status=true;
+                    }
+                    else {
+                  	  status=false;
+                    }
+  				
+  			} catch (Exception e) {
+  				// TODO: handle exception
+  				status=false;
+  				e.printStackTrace();
+  			}
+          	  return status;
+             }
+           
+           
+           
+           
+           
+           public List<User> getAllUsers(){
+        	   
+        	   String selectsqlall="select * from user";
+        	 return  jdbcTemplate.query(selectsqlall, new UserRowMapper());
+           }
+           
+           
+           
+           
+           public User getUserByEamil(String email) {
+        	   
+        	   String selectsql="select * from user where email=?";
+       	 return  jdbcTemplate.queryForObject(selectsql, new UserRowMapper(),email);
+        	   
+           }
+           public static final class UserRowMapper implements RowMapper<User>{
+
+			@Override
+			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+			
+				User user1=new User();
+				user1.setName(rs.getString("name"));
+				user1.setEmail(rs.getString("email"));
+				user1.setGender(rs.getString("gender"));
+				user1.setCity(rs.getString("city"));
+				
+				
+				
+				return user1;
+			}
+        	   
+           }
+           
+           
 	 
 }
